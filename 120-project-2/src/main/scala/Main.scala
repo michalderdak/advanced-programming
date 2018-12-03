57235a440665020bf485c5d7ab7594ad5d5a639d
Michal Derdak <michal.derdak@Michals-MacBook-Pro.local>
2018-12-03 16:34:09 +0100

    data processing, model training and evaluation added together with documentation

--- 120-project-2/src/main/scala/Main.scala
+++ 120-project-2/src/main/scala/Main.scala
@@ -3,10 +3,18 @@
 // Spark needs not to be installed (sbt takes care of it)
 
 import org.apache.spark.ml.feature.Tokenizer
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.ml.linalg.Vectors
+import scala.collection.mutable
 
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 object Main {
 
@@ -20,7 +28,7 @@ object Main {
 		.master  ("local[9]")
 		.getOrCreate
 
-  import spark.implicits._
+  	import spark.implicits._
 
 	val reviewSchema = StructType(Array(
 			StructField ("reviewText", StringType, nullable=false),
@@ -29,7 +37,7 @@ object Main {
 
 	// Read file and merge the text abd summary into a single text column
 
-	def loadReviews (path: String): Dataset[ParsedReview] =
+	def loadReviews (path: String): Dataset[ParsedReview] = {
 		spark
 			.read
 			.schema (reviewSchema)
@@ -42,29 +50,144 @@ object Main {
 			.withColumnRenamed ("_2", "text")
 			.withColumnRenamed ("_3", "overall")
 			.as[ParsedReview]
+	}
 
   // Load the GLoVe embeddings file
 
-  def loadGlove (path: String): Dataset[Embedding] =
-		spark
-			.read
-			.text (path)
-      .map  { _ getString 0 split " " }
-      .map  (r => (r.head, r.tail.toList.map (_.toDouble))) // yuck!
-			.withColumnRenamed ("_1", "word" )
-			.withColumnRenamed ("_2", "vec")
-			.as[Embedding]
+	def loadGlove (path: String): Dataset[Embedding] =  {
+			spark
+				.read
+				.text (path)
+		.map  { _ getString 0 split " " }
+		.map  (r => (r.head, r.tail.toList.map (_.toDouble))) // yuck!
+				.withColumnRenamed ("_1", "word" )
+				.withColumnRenamed ("_2", "vec")
+				.as[Embedding]
 
-  def main(args: Array[String]) = {
+	}
 
-    val glove  = loadGlove ("path/to/glove/file") // FIXME
-    val reviews = loadReviews ("path/to/amazon/reviews/file") // FIXME
+	def tokenize(reviews: Dataset[ParsedReview]): Dataset[Row] = {
+        val tokenizer = new Tokenizer()
+        tokenizer.setInputCol("text")
+        tokenizer.setOutputCol("words")
 
-    // replace the following with the project code
-    glove.show
-    reviews.show
+        val schema = StructType(
+            Array(
+                StructField ("id", IntegerType, nullable = false),
+                StructField ("words", DataTypes.createArrayType(StringType), nullable = false),
+                StructField ("label", DoubleType, nullable = false)
+            )
+        )
+ 
+        val encoder = RowEncoder(schema)
+ 
+        tokenizer.transform(reviews)
+        .select($"id", $"words", $"overall")
+            .map {
+                case Row(id: Int, words: Seq[String], label: Double) =>
+                    Row(id, words,
+                        if (label < 3) 0.0
+                        else if (label == 3) 1.0
+                        else 2.0
+                    )
+                } (encoder)
+    }
+ 
+  	def flattenTokens(reviews: Dataset[Row]): Dataset[ParsedReview] = {
+		reviews.flatMap {
+			case Row(id: Int, words: Seq[String], label: Double) => {
+				words.map(x => (id, x.toLowerCase(), label))
+			}
+		}.as[ParsedReview]
+  	}
 
-		spark.stop
-  }
+  	def vectorize(reviews: Dataset[ParsedReview], glove: Dataset[Embedding]): Dataset[Row] = {
+		val schema = StructType (
+			Array(
+				StructField("id", IntegerType, nullable = false),
+				StructField("attributes", DataTypes.createArrayType(DoubleType), nullable = false)
+			)
+		)
+		val encoder = RowEncoder(schema)
+
+		val aggregated = reviews.join(glove, reviews("_2") === glove("word"))	
+        val numUnique = aggregated.groupByKey(r => r.getAs[Int](0)).count
+
+		val vectorSum = aggregated
+			.drop("_2", "word", "_3", "label") 
+			.groupByKey(r => r.getAs[Int]("_1"))
+			.reduceGroups((a1, a2) => Row(a1.getInt(0), (a1.getSeq(1), a2.getSeq(1))
+			.zipped
+			.map((a1: Double, a2: Double) => a1 + a2)))
+			.map {
+				case (_, r: Row) => {
+					Row(r.getInt(0), r.getAs[mutable.WrappedArray[Double]](1).flatMap((x: Double) => Seq(x)))
+				}
+			} (encoder)
+	
+		val vecCount = vectorSum.join(numUnique, vectorSum("id") === numUnique("value")).drop("value")
+	
+		vecCount.map {
+			case Row(id: Int, attr: Seq[Double], num: Long) => {
+				Row(id, attr.map((x: Double) => x / num))
+			}
+		} (encoder)
+	}
+
+	def finalize(vectorizedReviews: Dataset[Row], tokenizedReviews: Dataset[Row]): DataFrame = {
+		vectorizedReviews
+			.join(tokenizedReviews, Seq("id"))
+			.drop("words", "id")
+			.as[(Array[Double], Double)]
+			.map { 
+				case (attributes: Array[Double], target: Double) => {
+					(Vectors.dense(attributes), target)
+				}
+			}
+			.withColumnRenamed ("_1", "features")
+			.withColumnRenamed ("_2", "label")
+	}
+
+	def train(df: DataFrame): Unit = {
+		val splits = df.randomSplit(Array(0.8, 0.2), seed = 1234L)
+		val train = splits(0)
+		val test = splits(1)
+		
+		val layers = Array[Int](50, 100, 3)
+
+		val trainer = new MultilayerPerceptronClassifier()
+			.setLayers(layers)
+			.setBlockSize(128)
+			.setSeed(1234L)
+			.setMaxIter(100)
+
+		val model = trainer.fit(train)
+
+		val result = model.transform(test)
+		val predictionAndLabels = result.select("prediction", "label")
+		val evaluator = new MulticlassClassificationEvaluator()
+			.setMetricName("accuracy")
+
+		println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+	}
+
+  	def main(args: Array[String]) = {
+
+		val glove  = loadGlove("./glove.6B.50d.txt") // FIXME
+		val rawReviews = loadReviews("./reviews.json") // FIXME 
+		
+		val tokenizedReviews = tokenize(rawReviews)
+		tokenizedReviews.show
+		val flattenReviews = flattenTokens(tokenizedReviews)
+		flattenReviews.show
+		val vectorizedReviews = vectorize(flattenReviews, glove)
+		vectorizedReviews.show
+		val reviews = finalize(vectorizedReviews, tokenizedReviews)
+		reviews.show
+
+		train(reviews)
+
+		spark.stop	
+	}
 
 }
